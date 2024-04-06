594931d0dddc3fc86aac7c421134efc9c6f6bc6f
Tilek <zikoshh89@gmail.com>
2024-04-06 12:02:49 +0700

    fix: routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,17 @@
 import { Header } from '../Header/Header';
 import Main from '../Main/Main';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Event from '../Event/Event';
 
 export default function App() {
   return (
     <>
       <Header />
-      <Main />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/event' element={<Event />} />
+      </Routes>
       {/* <Footer /> */}
     </>
   );
---
+++ src/components/Event/Event.tsx
@@ -0,0 +1,5 @@
+import './Event.css';
+
+export default function Event() {
+  return <div className='event'></div>;
+}
