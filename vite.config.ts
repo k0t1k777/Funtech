d4d1e40ae8a5bf47c8302013e3304636a3cc9453
Nickolai <lindeman072@mail.ru>
2024-04-17 07:53:02 +0400

    add: preddeploy14

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  base: "/",
+  base: "/funtech/",
   plugins: [
     react(),
     svgr({svgrOptions: {
