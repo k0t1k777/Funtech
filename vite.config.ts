5dfe7a0aca039b17f7bbcfb73d93ec5980435264
Nickolai <lindeman072@mail.ru>
2024-06-08 00:31:31 +0400

    fix: read m6

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  base: '.',
+  base: './',
   plugins: [react(), svgr({ svgrOptions: {} })],
 });
