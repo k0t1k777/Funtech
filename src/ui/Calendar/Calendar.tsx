09923cb815c4cb546fae561b8bfb81aa96dcd421
Nickolai <lindeman072@mail.ru>
2024-07-08 10:12:23 +0400

    fix/ deploy8

--- src/ui/Calendar/Calendar.tsx
+++ src/ui/Calendar/Calendar.tsx
@@ -53,12 +53,12 @@ export default function Calendar({
           '& .css-xlhjvr.Mui-selected': {
             backgroundColor: '#6750A4',
           },
-          // '& .css-xlhjvr.Mui-selected': {
-          //   backgroundColor: '#6750A4',
-          // },
-          // '& .css-xlhjvr.Mui-selected': {
-          //   backgroundColor: '#6750A4',
-          // },
+          '& css-6u8l34.Mui-selected': {
+            backgroundColor: '#6750A4',
+          },
+          '& css-6u8l34.Mui-selected:hover': {
+            backgroundColor: '#6750A4',
+          },
           '& .css-1uqqj9m': {
             backgroundColor: '#E8DEF8',
           },
